869f527d325388298c362787656c01b7d91f9c8b
heaubeau <mlspier@gmail.com>
2017-06-27 21:45:52 -0700

    More dev
    Changed model to include day table

--- moodtracker/admin.py
+++ moodtracker/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Mood, Event, Event_Type
+from .models import Day, Mood, Event, Event_Type
 
 admin.site.register(Mood)
 admin.site.register(Event)
 admin.site.register(Event_Type)
+admin.site.register(Day)
--- moodtracker/models.py
+++ moodtracker/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Day(models.Model):
+    date = models.DateField()
+    def __str__(self):
+        return str('%d/%d/%d' % (self.date.month, self.date.day, self.date.year))
+
 class Mood(models.Model):
     x_value = models.SmallIntegerField()
     y_value = models.SmallIntegerField()
     description = models.TextField()
     rec_date = models.DateTimeField('date recorded')
+    mood_day = models.ForeignKey(Day, default=1, on_delete=models.CASCADE)
     def __str__(self):
         return self.description
 
@@ -18,5 +24,6 @@ class Event(models.Model):
     rec_date = models.DateTimeField('date recorded')
     notes = models.TextField()
     event_type = models.ForeignKey(Event_Type, on_delete=models.CASCADE)
+    event_day = models.ForeignKey(Day, default=1, on_delete=models.CASCADE)
     def __str__(self):
         return (str(self.rec_date.month) + "/" + str(self.rec_date.day) + "/" + str(self.rec_date.year))
